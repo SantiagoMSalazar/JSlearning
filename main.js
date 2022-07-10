24982c72eb07b5e6502a4482d59d64d6786eced3
Santiago Salazar <santiago.miguel.salazar.p@gmail.com>
2022-07-09 21:14:48 -0500

    primer establecimiento de ejemplos para el uso de javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+var numero_1=2, numero_2=3;
+let dato = prompt("datos ingresar");
+window.alert("El resultado es " +(dato));
+if (dato!=numero_2){
+    window.alert("El resultado resultó distinto");
+}else{
+    window.alert("Es el mismo resultado");
+}
+
